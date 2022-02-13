11b2fafc76e1bebf7e1cd11b39ba9486cccd0bc1
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-02-12 18:02:28 -0700

    Client: Address initialization of client

--- client.cpp
+++ client.cpp
@@ -10,6 +10,13 @@
 
 int main(){
 
+    //address initialization
+    struct sockaddr_in address;
+    memset(&address, 0, sizeof(address));
+    address.sin_family= AF_INET;
+    address.sin_port= htons(7000);
+    address.sin_addr.s_addr = INADDR_ANY;
+
 
     return 0;
 }
