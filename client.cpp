ebee51ca2535a98418fc89c5e9ae75fdfc834879
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-02-12 17:38:45 -0700

    Initial file uploads

---
+++ client.cpp
@@ -0,0 +1,15 @@
+/* CLIENT SIDE CODE */
+
+//Socket programming imports & more!
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<unistd.h>	
+
+
+int main(){
+
+
+    return 0;
+}
