14ec639f29bc74f1603ef78fb7e2c629400fed38
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-02-12 17:46:50 -0700

    Server: address init and listening socket created

--- server.cpp
+++ server.cpp
@@ -10,6 +10,20 @@
 
 int main(){
 
+    //address initialization
+    struct sockaddr_in address;
+    memset(&address,0, sizeof(address));
+    address.sin_family= AF_INET;
+    address.sin_port= htons(7000);
+    address.sin_addr.s_addr= INADDR_ANY;
+
+     //listening socket creation
+    int listeningSocket;
+    listeningSocket= socket(AF_INET, SOCK_STREAM, 0);
+    if(listeningSocket == -1){
+        perror("Listening socket creation failed!");
+    }
+    printf("Listening socket created.\n");
 
     return 0;
 }
